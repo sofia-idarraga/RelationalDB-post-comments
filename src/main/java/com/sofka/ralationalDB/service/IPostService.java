ba2649432cecebb5387517409fb55f79840c406c
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-07-20 16:49:33 -0500

    finish service, creating controller

--- src/main/java/com/sofka/ralationalDB/service/IPostService.java
+++ src/main/java/com/sofka/ralationalDB/service/IPostService.java
@@ -1,4 +1,16 @@
 package com.sofka.ralationalDB.service;
 
+import com.sofka.ralationalDB.entity.Comment;
+import com.sofka.ralationalDB.entity.Post;
+
+import java.util.List;
+
 public interface IPostService {
+
+    Post createPost(Post post);
+    Post createComment(Comment comment);
+    void deletePost(Post post);
+    void deleteComment(Comment comment);
+
+    List<Post> findAllPost();
 }
--- src/main/java/com/sofka/ralationalDB/service/PostService.java
+++ src/main/java/com/sofka/ralationalDB/service/PostService.java
@@ -1,4 +1,52 @@
 package com.sofka.ralationalDB.service;
 
-public class PostService {
+import com.sofka.ralationalDB.entity.Comment;
+import com.sofka.ralationalDB.entity.Post;
+import com.sofka.ralationalDB.repository.CommentRepository;
+import com.sofka.ralationalDB.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService implements IPostService{
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post createComment(Comment comment) {
+        Post post = postRepository.findById(comment.getFK_post_id()).get();
+        post.addComment(comment);
+        commentRepository.save(comment);
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void deletePost(Post post) {
+        Post postToBeDeleted = postRepository.findById(post.getId()).get();
+        if(postToBeDeleted.getComments().size() > 0){
+            postToBeDeleted.getComments().forEach(comment -> commentRepository.deleteById(comment.getId()));
+        }
+        postRepository.deleteById(post.getId());
+    }
+
+    @Override
+    public void deleteComment(Comment comment) {
+        commentRepository.deleteById(comment.getId());
+    }
+
+    @Override
+    public List<Post> findAllPost() {
+        return null;
+    }
 }
