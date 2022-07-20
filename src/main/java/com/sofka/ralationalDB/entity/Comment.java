fae55499636ea65ab2916670e1f4d679584a65eb
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-07-20 16:06:36 -0500

    create repository, corrected the datasource.url

--- src/main/java/com/sofka/ralationalDB/entity/Comment.java
+++ src/main/java/com/sofka/ralationalDB/entity/Comment.java
@@ -1,4 +1,23 @@
 package com.sofka.ralationalDB.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Comment")
+@Table(name = "comment")
+@Data
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String message;
+    private Long FK_post_id;
+
+
 }
--- src/main/java/com/sofka/ralationalDB/entity/Post.java
+++ src/main/java/com/sofka/ralationalDB/entity/Post.java
@@ -1,4 +1,38 @@
 package com.sofka.ralationalDB.entity;
 
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Post")
+@Table(name = "post")
+@Data
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String message;
+    private String title;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post addComment(Comment comment){
+        this.comments.add(comment);
+        return this;
+    }
 }
